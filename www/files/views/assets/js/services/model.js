fbe1636f3010ce3e648cb32709b92be9179ba0b9
Feedbackhunter <aplicativo@feedbackhunter.com.br>
2015-06-17 17:43:07 -0300

    Aplicativo - Teste 1

---
+++ www/files/views/assets/js/services/model.js
@@ -0,0 +1,23 @@
+/**
+ * Data models
+ */
+Apperyio.Entity = new Apperyio.EntityFactory({
+    "Number": {
+        "type": "number"
+    },
+    "Boolean": {
+        "type": "boolean"
+    },
+    "String": {
+        "type": "string"
+    }
+});
+Apperyio.getModel = Apperyio.Entity.get.bind(Apperyio.Entity);
+
+/**
+ * Data storage
+ */
+Apperyio.storage = {
+
+    "resultado": new $a.SessionStorage("resultado", "String")
+};
