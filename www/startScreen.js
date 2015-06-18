22300a72ed0064b4d8598ce77bd37e65c4bd9290
Feedbackhunter <aplicativo@feedbackhunter.com.br>
2015-06-18 15:30:53 -0300

    0.0.0.6

--- www/startScreen.js
+++ www/startScreen.js
@@ -17,23 +17,12 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 
-        document.getElementById("startScreen_mobilebutton_2").addEventListener("click", function () {
-          cordova.plugins.barcodeScanner.scan(function (value) {
-            if (value >= "1" && value <= "500") {
-                localStorage.setItem("resultado", value.text);
-                window.location.replace("page1.html");
-                $url = 'http://feedbackhunter.esy.es/formulario/?f=' + value + '&uuid=' + device.uuid;
-                var ref = window.open($url, '_blank', "location=yes,closebuttoncaption=Fechar, width='400', height='800'");
-                ref.addEventListener('loadstart', function(event) {
-                    if (event.url.match("mobile/close")) {
-                        ref.close();
-                    }
-                });
-            }
-            
-          });
+        if (document.getElementById("botao").addEventListener) {
+            document.getElementById("botao").addEventListener("click", function () {
+          alert("HAUHAUHAUHA");
         });
-    },
+        }
+        },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
